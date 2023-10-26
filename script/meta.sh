4382f1f7b53f51c12ea206c172518b0a7ef466c1
Shen Li <lshen@i2r.a-star.edu.sg>
2023-10-26 14:58:06 +0800

    add --bn_momentum : stop running statistics in the inner loop --meta-lambd-lr: set lambd lr

--- script/meta.sh
+++ script/meta.sh
@@ -2,11 +2,11 @@
 
 python main.py \
   --dataset=PACS \
-  --save-path=Results/Ours/debug/PACS/resnets18_meta_gem_t_noTran \
+  --save-path=Results/Ours/debug/PACS/resnets18_sup1_gem_t_l1e1\
   --gpu=0 \
   --do-train=True \
   --meta-lr=1e-2 \
-  --meta-lambd-lr=1e-2 \
+  --meta-lambd-lr=1e-1 \
   --lr=1e-3 \
   --data-root=../../Datasets/DA \
   --replace \
@@ -25,68 +25,11 @@ python main.py \
   --mix-lambda=0.9 \
   --thresh=0 \
   --s=1 \
-  --train=tta_meta \
-  --eval=tta_meta \
+  --train=tta_meta_sup1 \
+  --eval=tta_meta_sup \
   --loss-names=gem-t
+  #--Transform
 
-python main.py \
-  --dataset=PACS \
-  --save-path=Results/Ours/debug/PACS/resnets18_meta_gem_t_l5e2_noTran \
-  --gpu=0 \
-  --do-train=True \
-  --meta-lr=1e-2 \
-  --meta-lambd-lr=5e-2 \
-  --lr=1e-3 \
-  --data-root=../../Datasets/DA \
-  --replace \
-  --meta-step=1 \
-  --meta-second-order=False \
-  --TTA-head em \
-  --model=DomainAdaptor \
-  --backbone=resnet18 \
-  --batch-size=64 \
-  --num-epoch=30 \
-  --exp-num 0 \
-  --start-time=0 \
-  --times=1 \
-  --fc-weight=10.0 \
-  --loader=meta \
-  --mix-lambda=0.9 \
-  --thresh=0 \
-  --s=1 \
-  --train=tta_meta \
-  --eval=tta_meta \
-  --loss-names=gem-t
-
-python main.py \
-  --dataset=PACS \
-  --save-path=Results/Ours/debug/PACS/resnets18_meta_gem_t_l5e2_m_noTran \
-  --gpu=0 \
-  --do-train=True \
-  --meta-lr=1e-2 \
-  --meta-lambd-lr=5e-2 \
-  --lr=1e-3 \
-  --data-root=../../Datasets/DA \
-  --replace \
-  --meta-step=1 \
-  --meta-second-order=False \
-  --TTA-head em \
-  --model=DomainAdaptor \
-  --backbone=resnet18 \
-  --batch-size=64 \
-  --num-epoch=30 \
-  --exp-num 0 \
-  --start-time=0 \
-  --times=1 \
-  --fc-weight=10.0 \
-  --loader=meta \
-  --mix-lambda=0.9 \
-  --thresh=0 \
-  --s=1 \
-  --train=tta_meta \
-  --eval=tta_meta \
-  --loss-names=gem-t \
-  --bn-momentum
 #python main.py \
 #  --dataset='OfficeHome' \
 #  --save-path='AdaBN/meta_norm_OfficeHome' \
