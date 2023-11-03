d25e68888f1f8082711ed6940f63ab06c43c7e44
Shen Li <lshen@i2r.a-star.edu.sg>
2023-11-03 11:24:20 +0800

    meta_sup.sh

--- script/meta_sup.sh
+++ script/meta_sup.sh
@@ -2,7 +2,7 @@
 
 python main.py \
   --dataset=PACS \
-  --save-path=Results/Ours/debug/PACS/resnets18_sup_Max_Lambda_gem_t_lr2e-4_meta_lr5e-2_maxlr1e-3_thresh075\
+  --save-path=Results/Ours/debug/PACS/resnets18_sup1_Max_Lambda_gem_t_lr2e-4_meta_lr5e-2_maxlr1e-3_thresh085\
   --gpu=0 \
   --do-train=True \
   --lr=2e-4 \
@@ -13,10 +13,10 @@ python main.py \
   --model=DomainAdaptor \
   --backbone=resnet18 \
   --batch-size=64 \
-  --num-epoch=30 \
-  --exp-num 0 \
+  --num-epoch=60 \
+  --exp-num -2 \
   --start-time=0 \
-  --times=1 \
+  --times=3 \
   --fc-weight=10.0 \
   --train=tta_meta_sup1 \
   --eval=tta_meta_sup \
@@ -25,12 +25,11 @@ python main.py \
   --loader=meta \
   --meta-lr=5e-2 \
   --meta-step=1 \
-  --thresh=0.75 \
+  --thresh=0.85 \
   --mix-lambda=0.75 \
   --meta-lambd-lr=5e-2 \
   --with-max \
-  --meta-max-lr=1e-3
-  #--domain_bn_shift
+  --meta-max-lr=1e-3 \
+  --domain_bn_shift
   #--no-inner-lambda
-  #--domain_bn_shift
   #--Transform
