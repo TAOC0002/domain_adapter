4bfd697a1f35006f45203814ab485fc4acc36c9a
Shen Li <lshen@i2r.a-star.edu.sg>
2023-11-03 09:30:58 +0800

    meta_sup.sh

--- script/meta_sup.sh
+++ script/meta_sup.sh
@@ -2,7 +2,7 @@
 
 python main.py \
   --dataset=PACS \
-  --save-path=Results/Ours/debug/PACS/resnets18_sup1_Max_Lambda_gem_t_lr2e-4_meta_lr5e-2_maxlr1e-3_thresh075\
+  --save-path=Results/Ours/debug/PACS/resnets18_sup_Max_Lambda_gem_t_lr2e-4_meta_lr5e-2_maxlr1e-3_thresh075\
   --gpu=0 \
   --do-train=True \
   --lr=2e-4 \
@@ -28,7 +28,7 @@ python main.py \
   --thresh=0.75 \
   --mix-lambda=0.75 \
   --meta-lambd-lr=5e-2 \
-  --with-max
+  --with-max \
   --meta-max-lr=1e-3
   #--domain_bn_shift
   #--no-inner-lambda
