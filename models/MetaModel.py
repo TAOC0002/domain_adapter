bf2ca6aa0a6c5df5f526fe3082de623d5cddaf67
taoc <taoc@i2r.a-star.edu.sg>
2023-11-03 09:42:17 +0800

    s3213

--- models/MetaModel.py
+++ models/MetaModel.py
@@ -111,17 +111,17 @@ def tta_meta_minimax(meta_model, train_data, lr, epoch, args, engine, mode):
         #optimizers.zero_grad()
         for data in split_data:
             optimizers.zero_grad()
-            # with higher.innerloop_ctx(meta_model, inner_opt_max, copy_initial_weights=False, track_higher_grads=True) as (fnet, opt_max):
-            #     if args.bn_momentum:
-            #         fnet.set_momentum(0)
-            #     for _ in range(args.meta_step):
-            #         unsup_loss, sup_loss = get_loss_and_acc(fnet(**data, train_mode='ft', step=_), running_loss, running_corrects, prefix=f'spt_max_')
-            #         opt_max.step(sup_loss-unsup_loss)
-            #     #optimizers.zero_grad()
-            #     losses = get_loss_and_acc(fnet(**data, train_mode='train'), running_loss, running_corrects, prefix=f'qry_max_')
-            #     losses[0].backward()
-            # optimizers.step()
-            # optimizers.zero_grad()
+            with higher.innerloop_ctx(meta_model, inner_opt_max, copy_initial_weights=False, track_higher_grads=True) as (fnet, opt_max):
+                if args.bn_momentum:
+                    fnet.set_momentum(0)
+                for _ in range(args.meta_step):
+                    unsup_loss, sup_loss = get_loss_and_acc(fnet(**data, train_mode='ft', step=_), running_loss, running_corrects, prefix=f'spt_max_')
+                    opt_max.step(sup_loss-unsup_loss)
+                #optimizers.zero_grad()
+                losses = get_loss_and_acc(fnet(**data, train_mode='train'), running_loss, running_corrects, prefix=f'qry_max_')
+                losses[0].backward()
+            optimizers.step()
+            optimizers.zero_grad()
             with higher.innerloop_ctx(meta_model, inner_opt_min, copy_initial_weights=False, track_higher_grads=True) as (fnet, opt_min):
                 if args.momentum:
                     fnet.set_momentum(0)
@@ -144,7 +144,7 @@ def tta_meta_minimax1(meta_model, train_data, lr, epoch, args, engine, mode):
     device, optimizers = engine.device, engine.optimizers
     running_loss, running_corrects = AverageMeterDict(), AverageMeterDict()
     meta_model.train()
-    inner_opt_max = get_new_optimizers(meta_model, lr=args.meta_lr, names=['bn'], param_names=['bias'], momentum=args.meta_second_order)
+    inner_opt_max = get_new_optimizers(meta_model, lr=args.meta_max_lr, names=['bn'], param_names=['bias'], momentum=args.meta_second_order)
     if args.no_inner_lambda:
         inner_opt_min = get_new_optimizers(meta_model, lr=args.meta_lr, names=['bn'], param_names=['bias', 'weight'], momentum=args.meta_second_order)
     else:
@@ -175,8 +175,6 @@ def tta_meta_minimax1(meta_model, train_data, lr, epoch, args, engine, mode):
                         opt_max.step(sup_loss-unsup_loss)
                     losses = get_loss_and_acc(fnet(**data, train_mode='train'), running_loss, running_corrects, prefix=f'qry_max_')
                     losses[0].backward()
-                optimizers.step()
-            optimizers.zero_grad()
             with higher.innerloop_ctx(meta_model, inner_opt_min, copy_initial_weights=False, track_higher_grads=True) as (fnet, opt_min):
                 if args.momentum:
                     fnet.set_momentum(0)
@@ -205,7 +203,7 @@ def tta_meta_minimax_test(meta_model, eval_data, lr, epoch, args, engine, mode):
     fast_model = copy.deepcopy(meta_model)
     meta_model.eval()
     fast_model.eval()
-    inner_opt_max = get_new_optimizers(meta_model, lr=args.meta_lr, names=['bn'], param_names=['bias'], momentum=args.meta_second_order)
+    inner_opt_max = get_new_optimizers(meta_model, lr=args.meta_max_lr, names=['bn'], param_names=['bias'], momentum=args.meta_second_order)
     if args.no_inner_lambda:
         inner_opt_min = get_new_optimizers(fast_model, lr=args.meta_lr, names=['bn'], param_names=['bias', 'weight'], momentum=args.meta_second_order)
     else:
